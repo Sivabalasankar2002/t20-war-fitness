fd80771a654b3550f76ac57ffa97bc4afec21e19
Sivabalasankar <sivabalasankar2002@gmail.com>
2025-09-15 11:35:40 +0530

    Update environment.prod.ts

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://52.172.154.119:3000'  // no trailing slash
+  apiUrl: 'http://52.172.154.119:3000/api'  // VM backend URL with /api prefix
 };
