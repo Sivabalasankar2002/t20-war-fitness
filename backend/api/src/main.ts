53e1182fd56b4277a3c7aec12ac1f6b75b82a9a0
Sivabalasankar <sivabalasankar2002@gmail.com>
2025-09-15 11:33:48 +0530

    Update main.ts

--- backend/api/src/main.ts
+++ backend/api/src/main.ts
@@ -3,16 +3,23 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-	const app = await NestFactory.create(AppModule);
-	app.enableCors();
-	app.setGlobalPrefix('api');
-	app.useGlobalPipes(
-		new ValidationPipe({
-			whitelist: true,
-			forbidNonWhitelisted: true,
-			transform: true,
-		}),
-	);
-	await app.listen(process.env.PORT || 3000);
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: ['https://icy-wave-03c902000.2.azurestaticapps.net'], // your frontend URL
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
+  app.setGlobalPrefix('api');
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
